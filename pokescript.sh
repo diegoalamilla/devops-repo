0e2e1293a00242ee9efd01cbb5a60839edfccaec
Diego Alamilla <90427832+diegoalamilla@users.noreply.github.com>
2025-02-16 13:30:08 -0600

    Update pokescript.sh

--- pokescript.sh
+++ pokescript.sh
@@ -6,7 +6,7 @@ if [ -z "$1" ]; then
 fi
 
 POKEMON=$1
-RESPONSE= $(curl "https://pokeapi.co/api/v2/pokemon/$POKEMON")
+RESPONSE=$(curl "https://pokeapi.co/api/v2/pokemon/$POKEMON")
 
 if [ -z "$RESPONSE" ]; then
     echo "Error: La respuesta está vacía"
@@ -23,12 +23,12 @@ ORDER=$(echo "$RESPONSE" | jq '.order')
 echo "$NAME (No. $ID)"
 echo "Id = $ID"
 echo "Weight = $WEIGHT"
-echo "Height = HEIGHT"
+echo "Height = $HEIGHT"
 
 FILENAME="pokemon_api_data.csv"
 
-if [ ! -f "$FILE" ]; then
-    echo "id,name,weight,height,order" > "$FILE"
+if [ ! -f "$FILENAME" ]; then
+    echo "id,name,weight,height,order" > "$FILENAME"
 fi
 
-echo "$ID,$NAME,$WEIGHT,$HEIGHT,$ORDER" >> "$FILE"
+echo "$ID,$NAME,$WEIGHT,$HEIGHT,$ORDER" >> "$FILENAME"
